a95303fbfcb3d43346481030e9e3f2b4dafc0d7a
zann <naim_khsn@live.co.uk>
2020-10-10 13:54:11 -0400

    first class

---
+++ src/firstfloor/AptOne.java
@@ -0,0 +1,12 @@
+package firstfloor;
+
+public class AptOne {
+	
+	public static void main(String[] args) {
+		System.out.println("Samsung");
+		String tv = "Samsung";
+		//dataType variable = value ;
+		System.out.println(tv);
+	}
+
+}
